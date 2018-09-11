6392f339aac6f355da0b441371311702b75c26a7
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-09-11 13:43:53 -0400

    Retrieve and parse resource tree

--- born-digital_dao/dump_dao_filenames.rb
+++ born-digital_dao/dump_dao_filenames.rb
@@ -1,19 +1,29 @@
-# Outputs a list of DO component filenames and AO component unique IDs for a 
-# given resource. Takes a resource ID as input and requires a YAML config file 
-# that includes credentials for an ASpace user with read access.
+# Outputs a CSV of DO component filenames and AO component unique IDs for a 
+# given resource. Takes a resource ID as input and requires a YAML file that 
+# includes some basic configuration info (see example_config.yml).
 
 require 'json'
 require 'csv'
 require 'yaml'
+require 'rest-client'
 
 conf = YAML::load_file(File.join(__dir__, 'config.yml'))
-username = conf["aspace_username"]
-password = conf["aspace_password"]
 
 resource_id = ARGV[0]
 
 if ARGV[0].to_i == 0
-  puts "Usage: ruby dump_dao_filenames.rb"
+  puts "Usage: ruby dump_dao_filenames.rb aspace_resource_id"
   exit
 end
+
+auth_resp = RestClient::Request.execute(method: :post, 
+                                       url: conf["aspace_base_uri"] + '/users/admin/login',
+                                       payload: { password: conf["aspace_password"] }
+)
+auth_resp_serialized = JSON.parse(auth_resp)
+session_id = auth_resp_serialized["session"]
+
+endpoint = conf["aspace_base_uri"] + "/repositories/" + conf["aspace_repo_id"] + "/resources/" + resource_id + "/tree"
+response = RestClient.get(endpoint, {"X-ArchivesSpace-Session": session_id})
+resource_tree = JSON.parse(response)
 
