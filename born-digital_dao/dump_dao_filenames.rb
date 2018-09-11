6d9d1ff54d789a49db33ef734716ab4ac6fc75b6
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-09-11 12:48:06 -0400

    Setup for DAO filename script

--- born-digital_dao/dump_dao_filenames.rb
+++ born-digital_dao/dump_dao_filenames.rb
@@ -1,3 +1,19 @@
+# Outputs a list of DO component filenames and AO component unique IDs for a 
+# given resource. Takes a resource ID as input and requires a YAML config file 
+# that includes credentials for an ASpace user with read access.
+
 require 'json'
 require 'csv'
+require 'yaml'
+
+conf = YAML::load_file(File.join(__dir__, 'config.yml'))
+username = conf["aspace_username"]
+password = conf["aspace_password"]
+
+resource_id = ARGV[0]
+
+if ARGV[0].to_i == 0
+  puts "Usage: ruby dump_dao_filenames.rb"
+  exit
+end
 
